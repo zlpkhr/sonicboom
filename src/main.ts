294be0fc27ff16b7960e34afebfd1b05eee69e78
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-09-30 01:06:19 +0500

    Setup cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { httpLogger } from "./logger.ts";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(httpLogger);
